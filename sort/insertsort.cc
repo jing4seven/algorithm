ad9843d4d804f01938c34dd39b3cee24e9d018b3
jing4seven <showkilling@gmail.com>
2014-04-12 22:17:36 +0800

    Add insert sort

---
+++ sort/insertsort.cc
@@ -0,0 +1,78 @@
+/*
+ * 插入排序
+ */
+#include <iostream>
+#include <vector>
+#include <time.h>
+#include <assert.h>
+
+#define random(x) (rand()%(x))
+
+using namespace std;
+
+void printsource(vector<int> &source) {
+    vector<int>::iterator itr = source.begin();
+    while (itr != source.end()) {
+        cout << *itr << "\t";
+        ++itr;
+    }
+
+    cout << "\n";
+}
+
+void isort(vector<int> &source) {
+    vector<int>::iterator iiter;
+    vector<int>::iterator jiter;
+    int tmp;
+
+    jiter = source.begin()+ 1;
+
+    while (jiter != source.end()) {
+        iiter = jiter-1;
+        tmp = *jiter;
+        // 将每个jiter所对应的元素跟已经排序好的
+        // 元素进行比较，通过交换，让其停留在合适
+        // 的位置
+        while (iiter >= source.begin()) {
+            if (*( iiter +1) < *iiter) {
+                swap(*iiter, *( iiter+1 ));
+            }
+            --iiter;
+        }
+        ++jiter;
+    }
+}
+
+int main() {
+    vector<int> source, testsource;
+    int i = 0;
+    int tmp = 0;
+
+    srand((int)time(0));
+    while (i < 100) {
+        tmp = random(1000);
+        source.push_back(tmp);
+        ++i;
+    }
+
+    testsource.assign(source.begin(), source.end());
+
+    cout << "source:" <<endl;
+    printsource(source);
+
+    // 使用STL自带的排序方法：
+    sort(testsource.begin(), testsource.end(), less<int>());
+    isort(source);
+
+    cout << "ordered source:" <<endl;
+    printsource(source);
+
+    cout << "valided source:" << endl;
+    printsource(testsource);
+
+    // 测试
+    assert(source == testsource);
+
+    return 0;
+
+}
