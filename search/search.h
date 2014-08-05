07c5f40e725e93d013b316fc4260ed57565db8d3
jing4seven <showkilling@gmail.com>
2014-08-05 23:40:03 +0800

    Add sample graph data structure for BFS & DFS.

--- search/search.h
+++ search/search.h
@@ -6,3 +6,22 @@ using namespace std;
 int BinarySearch(const vector<int> & s, const int k);
 
 ////////////////////////////////////////////////////////////////////////////////
+
+class GraphNode {
+public:
+    int color;
+    int distance;
+    GraphNode * pre;
+
+    GraphNode(int c, int d, GraphNode * p): color(c), distance(d), pre(p){}
+};
+
+class SampleGraph {
+public:
+    int vcount;
+    int ecount;
+
+    vector<GraphNode *> adjectList;
+};
+
+////////////////////////////////////////////////////////////////////////////////
